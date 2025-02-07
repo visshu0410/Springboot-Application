61bfcb01af4f820f7dd9c74d4e43d96e8524d3c2
Visshu0410 <vishal.gound@pinelabs.com>
2025-02-08 00:55:06 +0530

    testing

--- src/main/java/com/vishalgound/Springboot/Application/SpringbootApplication.java
+++ src/main/java/com/vishalgound/Springboot/Application/SpringbootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
